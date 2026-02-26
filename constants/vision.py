ed5d9e98fe099168ea820057d97f148bd5e4aa85
Sebastian Shinnick <sebastianshinnick@gmail.com>
2026-02-26 17:24:13 -0600

    visioning better

--- constants/vision.py
+++ constants/vision.py
@@ -51,7 +51,6 @@ class kCamera:
             ),
         )
 
-
 class kOdometry:
     MAX_VISION_AMBIGUITY = 0.3
     MAX_VISION_Z_ERROR = 0.75
@@ -61,7 +60,7 @@ class kOdometry:
     MAX_RPS = 2  # m/s
     USE_MEGATAG_2 = False
     
-    MIN_APRILTAGS = 1
+    MIN_APRILTAGS = 2
     MAX_ROTATIONAL_ERROR = 1
 
 
