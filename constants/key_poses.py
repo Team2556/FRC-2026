bf11ff968544086826fd98245ffd386ed12cfcea
Sebastian Shinnick <sebastianshinnick@gmail.com>
2026-02-28 16:48:00 -0600

    commit before merging again

--- constants/key_poses.py
+++ constants/key_poses.py
@@ -27,26 +27,3 @@ class kPoses:
     
     test_start_spot = Pose2d(8.328, 5.974, Rotation2d(pi))
     test_end_spot = Pose2d(9.677, 5.874, Rotation2d(pi))
-
-class kTranslations:
-    field_x = 16.54
-    field_y = 8.07
-    
-    bottom_left = Translation2d(0, 0)
-    bottom_right = Translation2d(16.54, 0)
-    top_left = Translation2d(0, 8.07)
-    top_right = Translation2d(16.54, 8.07)
-    
-    middle_left = Translation2d(0, 4.03)
-    middle_right = Translation2d(16.54, 4.03)
-    
-    alliance_hub = Translation2d(4, 4)
-    opposing_alliance_hub = Translation2d(12, 4)
-    
-    bottom_left_neutral_zone = Translation2d(4, 0)
-    bottom_right_neutral_zone = Translation2d(12, 0)
-    top_left_neutral_zone = Translation2d(4, 8.07)
-    top_right_neutral_zone = Translation2d(12, 8.07)
-    
-    midpoint = Translation2d(4.03, 8.27)
-    
