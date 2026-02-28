f3aa5fe611e61386dff937806b3f67366e732572
Unicrom <babypotato14.14@gmail.com>
2026-02-28 14:50:56 -0600

    Updated kMath and all references
    Now uses a more readable naming schema

--- constants/field.py
+++ constants/field.py
@@ -3,12 +3,12 @@ Holds all of the numeric values set for different subsystems and commands.
 This is not for tuning but for known values, such as a position of a field element.
 """
 
-from wpimath.geometry import Translation2d
+from wpimath.geometry import Translation2d, Pose2d
 from util.alliance_constant import AllianceConstant
 
 
 class kHub:
-    POS = Translation2d(4.0, 4.0)
+    POS = Pose2d(4.0, 4.0)
 
 class kField:
     WIDTH = 8.07
--- constants/shooter.py
+++ constants/shooter.py
@@ -1,6 +1,8 @@
 from phoenix6.configs import TalonFXConfiguration
 from wpimath.geometry import Transform2d, Rotation2d
 
+from constants.math import kMath
+
 class kShooterMotor:
     CAN_ID = 24
     _CONFIG = TalonFXConfiguration()
@@ -12,4 +14,4 @@ class kShooterMotor:
 class kShooterConfig:
     SHOOTER_OFFSET = Transform2d(-0.029566, -0.212725, Rotation2d())  # Meters
     SHOOTER_DIRECTION = 0 # 180 for Reverse
-    
+    WHEEL_RADIUS = 4 * kMath.MetersPerInch
