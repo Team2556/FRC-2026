23f49bd2863071d92ba6d1e25061726b8f95e246
Sebastian Shinnick <sebastianshinnick@gmail.com>
2026-03-13 21:41:13 -0500

    added: readme, hood improvements + homing, cleaning up outdated subsystem stuff

--- subsystems/shooter/shooter_hood.py
+++ subsystems/shooter/shooter_hood.py
@@ -1,27 +1,45 @@
 from commands2 import Subsystem
 from constants.shooter import kHoodMotor
 from phoenix6.hardware import TalonFX
-from phoenix6.controls import PositionVoltage
-from wpilib import SmartDashboard
+from phoenix6.controls import PositionVoltage, DutyCycleOut
+from phoenix6.signals import NeutralModeValue
 from util.editable_pid import EditablePID
+from util.nt_util import NTTable
 
 # Currently not tested yet. Also there is no "shooter hood" command as all the controlling for
 # this subsystem will be in the hub align command (eventually)
 class ShooterHood(Subsystem):
     def __init__(self):
-        self.shooter_motor = TalonFX(kHoodMotor.CAN_ID, "rio")
-        self.shooter_motor.configurator.apply(kHoodMotor._CONFIG)
+        self.hood_motor = TalonFX(kHoodMotor.CAN_ID, "rio")
+        self.hood_motor.configurator.apply(kHoodMotor._CONFIG)
+        self.hood_motor.setNeutralMode(NeutralModeValue.BRAKE)
         
         self.position_voltage = PositionVoltage(0)
+        self.home_voltage = DutyCycleOut(0)
+        
+        self.nt = NTTable("Hood")
+        self.nt.float("Hood Angle", 0.0)
+        self.editable_pid = EditablePID("Hood", self.hood_motor, kHoodMotor._CONFIG)
+    
+    def set_speed(self, speed):
+        self.hood_motor.set_control(self.home_voltage.with_output(speed))
     
     def increment(self, mult):
-        self.shooter_motor.set_control(self.shooter_motor.get() + (kHoodMotor.INCREMENT_AMOUNT * mult))
+        self.hood_motor.set_control(self.hood_motor.get() + ((kHoodMotor.INCREMENT_AMOUNT / 20) * mult))
     
     def set_position(self, position):
-        self.shooter_motor.set_control(self.position_voltage.with_position(position))
+        self.hood_motor.set_control(self.position_voltage.with_position(position))
+    
+    def is_hard_stopped(self):
+        # AI made this maybe test it
+        velocity = self.hood_motor.get_velocity().value
+        current = self.hood_motor.get_stator_current().value
+    
+        return abs(velocity) < 0.1 and current > 40
     
     def reset(self):
         self.set_position(0)
         
     def periodic(self):
-        SmartDashboard.putNumber("Hood Angle", self.shooter_motor.get())
+        self.nt.set("Hood Angle", self.hood_motor.get())
+        self.editable_pid.periodic()
---
+++ util/send_fms_data.py
@@ -0,0 +1,54 @@
+from wpilib import DriverStation
+from commands2 import Subsystem
+
+from util.nt_util import NTTable
+
+
+class SendFMSData(Subsystem):
+
+    intervals = [30, 55, 80, 105, 130]
+
+    def __init__(self):
+        self.intervals.append(999)
+        self.intervals.insert(0, 0)
+        
+        # Increasing value that is used for "Hub Active" light in this case
+        # Synchronizes with the teleop phases "1, 2, 3, 4" so it's easier to use
+        self.current_interval = 0
+
+        self.nt = NTTable("Extra FMS Data")
+        self.nt.float("Time Remaining")
+        self.nt.float("Time Until Next Phase")
+        self.nt.bool("Hub Active")
+
+    def periodic(self):
+        time_remaining = DriverStation.getMatchTime()
+
+        self.nt.set("Time Remaining", time_remaining)
+        for i in range(len(self.intervals) - 1):
+            if self.intervals[i + 1] > time_remaining:
+                time_until_next_phase = time_remaining - self.intervals[i]
+                self.current_interval = len(self.intervals) - 2 - i
+                self.nt.set("Time Until Next Phase", time_until_next_phase)
+                break
+        
+        self.nt.set("Hub Active", self.get_hub_active())
+    
+    def get_hub_active(self):
+        
+        auto_winner = DriverStation.getGameSpecificMessage()
+        if not auto_winner == "R" and not auto_winner == "B":
+            return True
+        
+        if self.current_interval < 1 or self.current_interval > 4:
+            return True
+        
+        is_hub_active = self.current_interval == 2 or self.current_interval == 4
+        
+        if DriverStation.getAlliance() == DriverStation.Alliance.kRed:
+            is_hub_active = not is_hub_active
+        
+        if auto_winner == "R":
+            is_hub_active = not is_hub_active
+        
+        return is_hub_active
