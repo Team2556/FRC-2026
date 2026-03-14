7ec3894f31540b18669f38ccfb57c84eefd4ee77
Sebastian Shinnick <sebastianshinnick@gmail.com>
2026-03-14 14:36:11 -0500

    removed controlled motor but transfer subsystem is better now YAY

---
+++ commands/run_transfer_motors.py
@@ -0,0 +1,15 @@
+import commands2
+
+from subsystems.transfer_sybsystem import TransferSubsystem
+
+class RunTransferCommand(commands2.Command):
+    def __init__(self, transfer_sybsystem: TransferSubsystem):
+        super().__init__()
+        self.transfer_sybsystem = transfer_sybsystem
+        self.addRequirements(transfer_sybsystem)
+        
+    def initialize(self):
+        self.transfer_sybsystem.activate()
+
+    def end(self, interrupted):
+        self.transfer_sybsystem.stop()
---
+++ subsystems/transfer_sybsystem.py
@@ -0,0 +1,57 @@
+from commands2 import Subsystem
+import phoenix6
+from phoenix6.controls import VelocityVoltage
+from phoenix6.hardware import TalonFX
+from constants.transfer import kSpindexer, kTrasnfer
+from constants.canbus import kCANId
+from util.editable_pid import EditablePID
+from util.nt_util import NTTable
+
+class TransferSubsystem(Subsystem):
+    def __init__(self):
+        self.spindex_motor = TalonFX(kCANId.hopper.SPINDEXER, "rio")
+        self.up_transfer_motor = TalonFX(kCANId.hopper.TRASNFER, "rio")
+        
+        self.spindex_motor.configurator.apply(kSpindexer._CONFIG)
+        self.up_transfer_motor.configurator.apply(kTrasnfer._CONFIG)
+        
+        self.spindex_motor.setNeutralMode(phoenix6.signals.NeutralModeValue.BRAKE)
+        self.up_transfer_motor.setNeutralMode(phoenix6.signals.NeutralModeValue.BRAKE)
+        
+        self.spindex_velocity_voltage = VelocityVoltage(0)
+        self.up_transfer_velocity_voltage = VelocityVoltage(0)
+        
+        # Network Tables stuff
+        self.nt = NTTable("Transfer")
+        self.nt.bool("Active", False)
+        
+        self.spindexer_nt = NTTable("Transfer").get_subtable("Spindexer")
+        self.spindexer_nt.float("RPM", 0.0)
+        self.spindexer_nt.float("Target RPM", kSpindexer.TARGET_RPM)
+        
+        self.up_transfer_nt = NTTable("Transfer").get_subtable("Up Transfer")
+        self.up_transfer_nt.float("RPM", 0.0)
+        self.up_transfer_nt.float("Target RPM", kTrasnfer.TARGET_RPM)
+        
+        self.spindex_editable_pid = EditablePID("Transfer/Spindexer", self.spindex_motor, kSpindexer._CONFIG)
+        self.up_transfer_editable_pid = EditablePID("Transfer/Up Transfer", self.up_transfer_motor, kTrasnfer._CONFIG)
+    
+    def activate(self):
+        self.spindex_motor.set_control(self.spindex_velocity_voltage.with_velocity(kSpindexer.TARGET_RPM))
+        self.up_transfer_motor.set_control(self.up_transfer_velocity_voltage.with_velocity(kTrasnfer.TARGET_RPM))
+        self.nt.set("Active", True)
+
+    def stop(self):
+        self.spindex_motor.set_control(self.spindex_velocity_voltage.with_velocity(0))
+        self.up_transfer_motor.set_control(self.up_transfer_velocity_voltage.with_velocity(0))
+        self.nt.set("Active", False)
+    
+    def periodic(self):    
+        self.spindexer_nt.set("RPM", self.spindex_motor.get_velocity().value)
+        kSpindexer.TARGET_RPM = self.spindexer_nt.get("Target RPM")
+        
+        self.up_transfer_nt.set("RPM", self.up_transfer_motor.get_velocity().value)
+        kTrasnfer.TARGET_RPM = self.up_transfer_nt.get("Target RPM")
+        
+        self.spindex_editable_pid.periodic()
+        self.up_transfer_editable_pid.periodic()
