971eff9001d18620c6b72fd34b234160ac07d9b7
Unicrom <babypotato14.14@gmail.com>
2026-03-17 12:42:20 -0500

    Fixed is_charged flag

--- subsystems/shooter/dual_shooter.py
+++ subsystems/shooter/dual_shooter.py
@@ -18,7 +18,7 @@ class ShooterState(Enum):
     IDLE = 0
     """Coasting ~1000 RPM"""
     ENABLED = 1
-    """At target RPM (or increaing to)"""
+    """At target RPM (or increasing to)"""
 
 
 class DualMotorShooter(commands2.Subsystem):
@@ -42,10 +42,16 @@ class DualMotorShooter(commands2.Subsystem):
             )
         )
 
-        self.idle_request = VelocityVoltage(velocity=kShooterMotor.IDLE_RPM / 60, slot=0)
+        self.idle_request = VelocityVoltage(
+            velocity=kShooterMotor.IDLE_RPM / 60, slot=0
+        )
         self.coast_request = NeutralOut()
-        self.charge_request = VelocityVoltage(velocity=kShooterMotor.TARGET_RPM / 60, slot=1)
-        self.shoot_request = VelocityVoltage(velocity=kShooterMotor.TARGET_RPM / 60, slot=2)
+        self.charge_request = VelocityVoltage(
+            velocity=kShooterMotor.TARGET_RPM / 60, slot=1
+        )
+        self.shoot_request = VelocityVoltage(
+            velocity=kShooterMotor.TARGET_RPM / 60, slot=2
+        )
 
         self._state: ShooterState = ShooterState.IDLE
         self.is_charged = False
@@ -58,10 +64,18 @@ class DualMotorShooter(commands2.Subsystem):
         self.nt_sub.float("RPM", 0.0)
         self.nt_sub.float("Target RPM", default=kShooterMotor.TARGET_RPM)
         self.nt_sub.float("Idle RPM", default=kShooterMotor.IDLE_RPM)
-        self.nt_sub.float("Reach Target Velocity Error", default=kShooterMotor.REACH_TARGET_VELOCITY_ERROR)
+        self.nt_sub.float(
+            "Reach Target Velocity Error",
+            default=kShooterMotor.REACH_TARGET_VELOCITY_ERROR,
+        )
 
         self.editable_PID = EditablePID(
-            "Shooter/Motor", self._top_motor, self.cfg, use_slot0=True, use_slot1=True, use_slot2=True
+            "Shooter/Motor",
+            self._top_motor,
+            self.cfg,
+            use_slot0=True,
+            use_slot1=True,
+            use_slot2=True,
         )
 
     def disable(self) -> None:
@@ -74,28 +88,38 @@ class DualMotorShooter(commands2.Subsystem):
 
     def periodic(self):
         self.editable_PID.periodic()
-        
+
         motor_velocity_rpm = self._top_motor.get_velocity().value * 60
         if self._state == ShooterState.IDLE:
-            self._top_motor.set_control(self.idle_request)
-            
-            self.nt.set('State', 'IDLE')
+            self._top_motor.set_control(
+                self.idle_request.with_velocity(kShooterMotor.IDLE_RPM)
+            )
+
+            self.nt.set("State", "IDLE")
 
         elif self._state == ShooterState.ENABLED and not self.is_charged:
-            self._top_motor.set_control(self.charge_request)
+            self._top_motor.set_control(
+                self.charge_request.with_velocity(kShooterMotor.TARGET_RPM)
+            )
+            self.is_charged = (
+                abs(motor_velocity_rpm - kShooterMotor.TARGET_RPM)
+                < kShooterMotor.REACH_TARGET_VELOCITY_ERROR
+            )
 
-            self.is_charged = (motor_velocity_rpm > kShooterMotor.TARGET_RPM - kShooterMotor.REACH_TARGET_VELOCITY_ERROR)
-            
-            self.nt.set('State', 'CHARGING')
+            self.nt.set("State", "CHARGING")
 
         elif self._state == ShooterState.ENABLED and self.is_charged:
-            self._top_motor.set_control(self.shoot_request)
-            
-            self.nt.set('State', 'CHARGED')
+            self._top_motor.set_control(
+                self.shoot_request.with_velocity(kShooterMotor.TARGET_RPM)
+            )
+
+            self.nt.set("State", "CHARGED")
 
         self.nt.set("Motor Charged", self.is_charged)
         self.nt_sub.set("RPM", motor_velocity_rpm)
-        
-        kShooterMotor.IDLE_RPM = self.nt_sub.get('Idle RPM')
-        kShooterMotor.TARGET_RPM = self.nt_sub.get('Target RPM')
-        kShooterMotor.REACH_TARGET_VELOCITY_ERROR = self.nt_sub.get('Reach Target Velocity Error')
+
+        kShooterMotor.IDLE_RPM = self.nt_sub.get("Idle RPM")
+        kShooterMotor.TARGET_RPM = self.nt_sub.get("Target RPM")
+        kShooterMotor.REACH_TARGET_VELOCITY_ERROR = self.nt_sub.get(
+            "Reach Target Velocity Error"
+        )
