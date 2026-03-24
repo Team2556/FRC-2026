e1d0d0e4d611c3b80ee7a92360f9209248bda469
Unicrom <babypotato14.14@gmail.com>
2026-03-24 17:18:01 -0500

    Updated controller bidings

--- robotcontainer.py
+++ robotcontainer.py
@@ -4,65 +4,37 @@
 # the WPILib BSD license file in the root directory of this project.
 #
 
-from commands.auto_align import align_with_controller
-from commands.drive import drive_commands
 from util.custom_controller import XboxController
 from util.send_fms_data import SendFMSData
 from util.auto_chooser import AutoChooser
-from util.robot_zone_checker import RobotZoneChecker
-from util.tune_with_controller import TuneShooterSpeed, TuneAlignAngle, TuneHoodAngle
 
+from commands.auto_align import align_with_controller
+from commands.drive import drive_commands
 from commands.vision import vision_odometry
-from commands.path_commands import custom_path_commands, go_back_with_path, go_to_shooting_spot, drive_to_a_spot
-from commands.transfer.run_transfer_motors import RunTransferCommand, ReverseTransferCommand, SpindexOnlyCommand
+from commands.path_commands import custom_path_commands
+from commands.transfer.run_transfer_motors import RunTransferCommand
 from commands.intake.intake_commands import (
-    IntakeCommandDeploy,
-    IntakeCommandUndeploy,
-    IntakeForceRetract,
     IntakeCommandManualForward,
     IntakeCommandManualReverse,
     IntakeDefaultCommand,
     IntakeRollerForward,
     IntakeRollerBackward,
 )
-from commands.climb.climb import ClimbDown, ClimbUp
 from commands.shooter import shooter_commands, hood_commands
-from commands.drive.drive_commands import (
-    OverrideRotation,
-    SetOdometryBackLeft,
-    SetOdometryBackRight,
-)
 
 from constants.vision import kCamera
 from constants.drive import kDriveConfig
-from constants.led import kLED
-from constants.key_poses import kPoses
 
 from subsystems.drivetrain import drivetrain
 from subsystems.vision import mono_limelight
 from subsystems.intake.intake import IntakeSubsystem
-from subsystems.climb.climb_subsystem import ClimbSubsystem
 from subsystems.trasnfer.transfer_subsystem import TransferSubsystem
-
 from subsystems.shooter.shooter_hood import ShooterHood, HoodStates
 from subsystems.led.LED_controller import CANdleLEDController
 from subsystems.shooter.dual_shooter import DualMotorShooter
 
-from commands2 import (
-    ParallelCommandGroup,
-    RunCommand,
-    cmd,
-    InstantCommand,
-    ConditionalCommand,
-    SequentialCommandGroup,
-    ParallelRaceGroup,
-    WaitCommand
-)
+from commands2 import cmd
 from commands2.button import Trigger
-
-from util.robot_zone_checker import RobotZoneChecker
-from util.flip_util import FlipUtil
-from constants.field import kHub, kPassSpots
 
 
 class RobotContainer:
@@ -97,11 +69,6 @@ class RobotContainer:
         self.auto_chooser = AutoChooser(self.custom_path_commands.make_autos())
         self.auto_chooser.make_dropdown()
 
-        # In-game tuner stuff:
-        # self.tune_shooter_speed = TuneShooterSpeed(self._controller_2)
-        self.tune_hood_angle = TuneHoodAngle(self._controller_2)
-        # self.tune_align_angle = TuneAlignAngle(self._controller_2)
-
         self.configureButtonBindings()
 
     def configureButtonBindings(self) -> None:
@@ -120,31 +87,12 @@ class RobotContainer:
             drive_commands.ControllerDrive(self._drivetrain, self._controller_1)
         )
 
-        # .negate() guards prevent firing when both bumpers are held (that's intake) _FCC_
-        # self._controller_1.rightBumper().and_(
-        #     self._controller_1.leftBumper().negate()
-        # ).whileTrue(
-        #     ParallelCommandGroup(
-        #         RunTransferCommand(self.transfer_subsystem, self.shooter_subsystem),
-        #         shooter_commands.EnableShooter(self.shooter_subsystem),
-        #     )
-        # )
-
         self._controller_1.rightTrigger().whileTrue(
             align_with_controller.ConditionalAlignAndShoot(
                 self._drivetrain,
                 self.shooter_subsystem,
                 self.transfer_subsystem,
                 self.hood_subsystem,
-            )
-        )
-
-        # self._controller_1.leftBumper().whileTrue(go_back_with_path.GoBackWithPath(self._drivetrain))
-        
-        self._controller_1.leftBumper().whileTrue(
-            ParallelCommandGroup(
-                go_to_shooting_spot.GoToShootingSpot(self._drivetrain),
-                shooter_commands.EnableShooter(self.shooter_subsystem)
             )
         )
 
@@ -157,64 +105,14 @@ class RobotContainer:
             )
         )
 
-        self._controller_1.x().and_(self._controller_1.rightBumper().not_()).whileTrue(
-            ParallelCommandGroup(
-                self.custom_path_commands.left_trench,
-                IntakeCommandManualForward(self.intake_subsystem),
-            )
-        )
-        
-        self._controller_1.x().and_(self._controller_1.rightBumper()).whileTrue(
-            drive_to_a_spot.DriveToASpot(self._drivetrain, target_pose=kPoses.left_trench_feed).with_precise_values()
-        )
-        
-        self._controller_1.a().whileTrue(
-            self.custom_path_commands.left_bump
-        )
-        
-        self._controller_1.y().whileTrue(
-            self.custom_path_commands.right_bump
-        )
-        
-        self._controller_1.b().and_(self._controller_1.rightBumper().not_()).whileTrue(
-            ParallelCommandGroup(
-                self.custom_path_commands.right_trench,
-                IntakeCommandManualForward(self.intake_subsystem),
-            )
-        )
-        
-        self._controller_1.b().and_(self._controller_1.rightBumper()).whileTrue(
-            drive_to_a_spot.DriveToASpot(self._drivetrain, target_pose=kPoses.right_trench_feed).with_precise_values()
-        )
-        
-        self._controller_1.povUp().onTrue(
-            OverrideRotation(self._drivetrain)
-        )
-        self._controller_1.povLeft().onTrue(
-            SetOdometryBackLeft(self._drivetrain)
-        )
-        self._controller_1.povRight().onTrue(
-            SetOdometryBackRight(self._drivetrain)
-        )
-
         # CONTROLLER 2
 
-        # =========================
-        #        TESTING ONLY
-        # self.hood_subsystem.setDefaultCommand(
-        #     hood_commands.ManualShooterHood(
-        #         self.hood_subsystem,
-        #         self._controller_2,
-        #         self._get_hood_pose_and_target,
-        #     )
-        # )
         # Retracts hood in danger zone (bumps/trench); interrupts default command _FCC_
         self.hood_subsystem.setDefaultCommand(
             hood_commands.UpdateHoodPositionVariable(
                 self.hood_subsystem, self._drivetrain
             )
         )
-        # =========================
 
         # TRANSFER + SHOOTER
 
@@ -222,12 +120,6 @@ class RobotContainer:
 
         self._controller_2.y().whileTrue(
             shooter_commands.EnableShooter(self.shooter_subsystem)
-        )
-
-        self._controller_2.x().whileTrue(SpindexOnlyCommand(self.transfer_subsystem))
-
-        self._controller_2.a().whileTrue(
-            ReverseTransferCommand(self.transfer_subsystem)
         )
 
         # INTAKE
@@ -250,56 +142,6 @@ class RobotContainer:
         self._controller_2.povUp().onTrue(
             hood_commands.ResetShooterHood(self.hood_subsystem)
         )
-
-        self._controller_2.povLeft().whileTrue(
-            IntakeRollerBackward(self.intake_subsystem)
-        )
-
-        self._controller_2.povDown().whileTrue(
-            hood_commands.SetShooterHoodState(
-                self.hood_subsystem, HoodStates.OUTER_RING
-            )
-        )
-        # self._controller_2.povDown().onFalse(hood_commands.ToggleOffOverrideHood())
-
-        # self._controller_2.leftStick().onTrue(cmd.runOnce(lambda: self.tune_shooter_speed.reset()))
-        self._controller_2.leftStick().onTrue(cmd.runOnce(lambda: self.tune_hood_angle.reset()))
-        # self._controller_2.rightStick().onTrue(cmd.runOnce(lambda: self.tune_align_angle.reset()))
-
-        # self._controller_2.povDown().onTrue(IntakeForceRetract(self.intake_subsystem))
-
-        # self._controller_2.povUp().onTrue(ClimbUp(self.climb_subsystem))
-
-        # self._controller_2.povDown().onTrue(ClimbDown(self.climb_subsystem))
-
-        # Controller 1: both bumpers together — intake deploy
-        # _c1_intake = self._controller_1.leftBumper().and_(
-        #     self._controller_1.rightBumper()
-        # )
-        # _c1_intake.onTrue(IntakeCommandDeploy(self.intake_subsystem))
-        # _c1_intake.onFalse(IntakeCommandUndeploy(self.intake_subsystem))
-
-        # Dev only: Back + Start force-pushes all dashboard PID values to motors
-        (self._controller_1.back().and_(self._controller_1.start())).onTrue(
-            InstantCommand(self._force_apply_all_pids)
-        )
-
-    def _get_hood_pose_and_target(self):
-        # Picks hub or pass spot based on field zone; keeps drivetrain out of hood commands _FCC_
-        pose = self._drivetrain.get_state().pose
-        if RobotZoneChecker.is_in_left_passing_zone(pose):
-            return pose, FlipUtil.fieldPose(kPassSpots.PASS_SPOT_LEFT)
-        if RobotZoneChecker.is_in_right_passing_zone(pose):
-            return pose, FlipUtil.fieldPose(kPassSpots.PASS_SPOT_RIGHT)
-        return pose, FlipUtil.fieldPose(kHub.POS)
-
-    def _force_apply_all_pids(self):
-        self.intake_subsystem.pivot_editable_pid.force_apply()
-        self.intake_subsystem.roller_editable_pid.force_apply()
-        self.transfer_subsystem.spindex_editable_pid.force_apply()
-        self.transfer_subsystem.up_transfer_editable_pid.force_apply()
-        self.hood_subsystem.editable_pid.force_apply()
-        self.shooter_subsystem.editable_PID.force_apply()
 
     def getAutonomousCommand(self):
         # UNCOMMENT THIS OR ELSE
